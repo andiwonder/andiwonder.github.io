079bd56ef10727f55c61cd59772e95c664c9bf44
andiwonder <skasliwal12@gmail.com>
2022-09-28 10:52:35 -0400

    delete old stuff and push new stuff

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import RockPaperScissorsGame from './components/Rock-Paper-Scissors-game/';
+import EcommerceProductPage from './components/E-commerce-product-page/';
+import IntroDropdownNavigation from './components/Intro-doprdown-navigation/';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<EcommerceProductPage />} />
+          <Route path="/intro-dropdown-navigation" element={<IntroDropdownNavigation />} />
+          <Route path="/rock-paper-scissors-game" element={<RockPaperScissorsGame />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
